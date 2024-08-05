e863c6d55a1adea68c2201273588e2c5796ce41f
nen02 <riverarenen02@gmail.com>
2024-08-06 02:43:44 +0800

    fixed footer on mobile devices

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,10 +45,10 @@ const links: LinkProps[] = [
 
 function Footer() {
   return (
-    <div className="bg-[#F6F5F4]">
-      <div className="container max-w-screen-lg mx-auto flex justify-between items-center border-t-[1px] border-t-gray-300 h-20">
+    <div className="bg-[#F6F5F4] py-5 border-t-[1px] border-t-gray-300">
+      <div className="container max-w-screen-lg mx-auto grid grid-cols-2 max-sm:grid-cols-1 justify-between items-center max-lg:px-2 gap-2">
         <p>© 2024 All rights reserved.</p>
-        <div className="flex justify-end items-center gap-x-5 max-md:px-2">
+        <div className="flex justify-end max-sm:justify-start items-center gap-x-5">
           {links.map((link) => (
             <Link
               key={link.key}
