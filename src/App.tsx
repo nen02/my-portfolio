c69209109685a942c3523a151fb93138621eaf32
nen02 <riverarenen02@gmail.com>
2024-10-28 22:45:08 +0800

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { CallToAction, Footer, Hero, Services, TechStack } from './components';
+import { CallToAction, Footer, Hero, Services, TechStack } from "./components";
 
 function App() {
   return (
-    <div className='content'>
+    <div className="content">
       <Hero />
       <TechStack />
       <Services />
